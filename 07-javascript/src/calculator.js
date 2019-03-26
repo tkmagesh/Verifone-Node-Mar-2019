192d2acc02e704f1bd8f07d1ea99c54b54a3ff5d
Magesh <tkmagesh77@gmail.com>
2019-03-26 11:01:09 +0530

    calcular solution - part - 1

--- 07-javascript/src/calculator.js
+++ 07-javascript/src/calculator.js
@@ -1,6 +1,25 @@
 function add(x,y){
 	function parseArg(n){
+		if (Array.isArray(n)){
+			var result = 0;
+			for(var index = 0, count = n.length; index < count; index++)
+				result += parseArg(n[index]);
+			return result;
+		}
+		if (typeof n === 'function') return parseArg(n());
 		return isNaN(n) ? 0 : parseInt(n);
 	}
-	return parseArg(x) + parseArg(y);
+	var result = 0;
+	for(var index =0, count = arguments.length; index < count; index++){
+		result += parseArg(arguments[index]);
+	}
+	return result;
 }
+
+/*
+	arguments
+		- array like object
+			indexer
+			length
+	this
+*/
