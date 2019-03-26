a9d7e30d9aed2ec39ea3c8973e9a4d0582cf490d
Magesh <tkmagesh77@gmail.com>
2019-03-26 13:05:46 +0530

    click tracker - closures

--- 07-javascript/src/calculator.js
+++ 07-javascript/src/calculator.js
@@ -1,19 +1,10 @@
 function add(x,y){
 	function parseArg(n){
-		if (Array.isArray(n)){
-			var result = 0;
-			for(var index = 0, count = n.length; index < count; index++)
-				result += parseArg(n[index]);
-			return result;
-		}
+		if (Array.isArray(n)) return add.apply(undefined, n);
 		if (typeof n === 'function') return parseArg(n());
 		return isNaN(n) ? 0 : parseInt(n);
 	}
-	var result = 0;
-	for(var index =0, count = arguments.length; index < count; index++){
-		result += parseArg(arguments[index]);
-	}
-	return result;
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
 
 /*
@@ -22,4 +13,15 @@ function add(x,y){
 			indexer
 			length
 	this
+
+	As a method of an obj
+		this -> obj
+
+	As a function
+		this -> global (window)
+
+	using the call method
+
+	using the apply method
+
 */
